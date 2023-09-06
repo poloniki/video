715be5166ceb9497b7ac1a35f04bd73e0234c52f
Nikita Polovinkin <poloniki@ya.ru>
2023-09-06 23:34:55 +0200

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,17 +14,32 @@ import numpy as np
 
 
 def get_coordinates_of_clusters(frame):
-    # image_resized = imutils.resize(frame, width=1000)
     image_greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     image_blurred = cv2.GaussianBlur(image_greyscale, (5, 5), 0)
     image_canny_kernel = cv2.Canny(image_blurred, 50, 150)
     contours, _ = cv2.findContours(
         image_canny_kernel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
+
+    preprocessed_image_size = image_greyscale.shape[0] * image_greyscale.shape[1]
+
     coordinates = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        coordinates.append((x, y, w, h))
+        area = w * h
+        aspect_ratio_w_h = w / h
+        aspect_ratio_h_w = h / w
+
+        # Change these thresholds as per your requirements
+        area_threshold = 0.01 * preprocessed_image_size
+        aspect_ratio_threshold = 0.25
+
+        if (
+            area > area_threshold
+            and aspect_ratio_w_h > aspect_ratio_threshold
+            and aspect_ratio_h_w > aspect_ratio_threshold
+        ):
+            coordinates.append((x, y, w, h))
 
     return coordinates
 
@@ -33,16 +48,31 @@ class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         height, width = img.shape[:2]  # get dimensions for drawing rectangles
-        print(f"Frame dimensions: Width = {width}, Height = {height}")
 
         coordinates = get_coordinates_of_clusters(img)
-        print(f"Coordinates: {coordinates}")
+        print(coordinates)
 
-        for x, y, w, h in coordinates:
-            print(f"Drawing rectangle at x={x}, y={y}, w={w}, h={h}")
+        # Sort coordinates based on area in descending order
+        sorted_coordinates = sorted(
+            coordinates, key=lambda x: x[2] * x[3], reverse=True
+        )
+
+        for i, (x, y, w, h) in enumerate(sorted_coordinates):
             top_left = (x, y)
             bottom_right = (x + w, y + h)
             cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+            label = "Player Hand" if i == 0 else "Dealer Hand" if i == 1 else ""
+            if label:
+                cv2.putText(
+                    img,
+                    label,
+                    (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 255, 255),
+                    1,
+                )
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
